a286287fb6261dc58b3e8dd6247c54d75629b8a5
v-lizhengfan <serendipity_yihu@163.com>
2025-11-10 17:21:36 +0800

    - 多智能体，仿照医院

--- src/test/java/com/yihu/agent/RAGTest.java
+++ src/test/java/com/yihu/agent/RAGTest.java
@@ -282,50 +282,5 @@ public class RAGTest {
 
     }
 
-    @@Test
-     public void MetaDataTest() throws URISyntaxException {
-
-        Document document = loadDocument(toPath(documentPath), new TextDocumentParser());
-
-        EmbeddingModel embeddingModel = new BgeSmallEnV15QuantizedEmbeddingModel();
-
-        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
-
-        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
-                .documentSplitter(DocumentSplitters.recursive(300, 0))
-                .embeddingModel(embeddingModel)
-                .embeddingStore(embeddingStore)
-                .build();
-
-        ingestor.ingest(document);
-
-        ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
-                .embeddingStore(embeddingStore)
-                .embeddingModel(embeddingModel)
-                .build();
-
-        // Each retrieved segment should include "file_name" and "index" metadata values in the prompt
-        ContentInjector contentInjector = DefaultContentInjector.builder()
-                // .promptTemplate(...) // Formatting can also be changed
-                .metadataKeysToInclude(asList("file_name", "index"))
-                .build();
-
-        RetrievalAugmentor retrievalAugmentor = DefaultRetrievalAugmentor.builder()
-                .contentRetriever(contentRetriever)
-                .contentInjector(contentInjector)
-                .build();
-
-        ChatModel chatModel = OpenAiChatModel.builder()
-                .apiKey(OPENAI_API_KEY)
-                .modelName(GPT_4_O_MINI)
-                .logRequests(true)
-                .build();
-
-        return AiServices.builder(Assistant.class)
-                .chatModel(chatModel)
-                .retrievalAugmentor(retrievalAugmentor)
-                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
-                .build();
-    }
 
 }
